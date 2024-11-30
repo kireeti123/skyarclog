8712e8431eb6ceb4acfc112f5bc4d90f3a4d41eb
kireeti123 <kireeti.k.k@gmail.com>
2024-11-30 22:41:47 +0530

    Rename package to skyarclog

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
+"""
+Setup configuration for skyarclog package.
+"""
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("advanced_logging/requirements.txt", "r", encoding="utf-8") as fh:
+with open("skyarclog/requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
-    name="advanced-logging-framework",
+    name="skyarclog",
     version="1.0.0",
     author="Krishna Kireeti Kompella",
     author_email="kireeti.k.k@gmail.com",
     description="A comprehensive logging framework with multiple formatters and listeners",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kireeti123/advanced-logging",
+    url="https://github.com/kireeti123/skyarclog",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: System :: Logging",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
